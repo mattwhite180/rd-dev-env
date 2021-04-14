34944aa676a0482d3491b787b464b0122465f617
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2021-04-14 12:33:01 -0600

    minor changes

--- web/script.sh
+++ web/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update -y
-apt-get install -y apt-utils make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev  python-openssl git npm  apt-utils openssl libxmlsec1 pkg-config freetds-dev postgresql apt-utils unixodbc-dev libgssapi-krb5-2 postgresql postgresql-contrib libpq-dev
+apt-get install -y apt-utils nodeenv make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev  python-openssl git npm  apt-utils openssl libxmlsec1 pkg-config freetds-dev postgresql apt-utils unixodbc-dev libgssapi-krb5-2 postgresql postgresql-contrib libpq-dev
 
 echo 'PATH="$/pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
@@ -37,11 +37,15 @@ echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 
 source ~/.bashrc
 
- #PGPASSWORD=test1234 psql -h 172.20.0.2 -U rd postgres --command "CREATE USER rd WITH PASSWORD 'test1234';" &&\
- #PGPASSWORD=test1234 psql -h 172.20.0.2 -U rd postgres --command "CREATE DATABASE rentdynamics WITH OWNER rd;" &&\
- #PGPASSWORD=test1234 psql -h 172.20.0.2 -U rd postgres --command "CREATE DATABASE rentplus WITH OWNER rd;" &&\
- #PGPASSWORD=test1234 psql -h 172.20.0.2 -U rd postgres --command "ALTER USER rd WITH SUPERUSER;"
-
+ # PGPASSWORD=test1234 psql -h 172.20.0.2 -U rd postgres --command "CREATE USER rd WITH PASSWORD 'test1234';" &&\
+ # PGPASSWORD=test1234 psql -h 172.20.0.2 -U rd postgres --command "CREATE DATABASE rentdynamics WITH OWNER rd;" &&\
+ # PGPASSWORD=test1234 psql -h 172.20.0.2 -U rd postgres --command "CREATE DATABASE rentplus WITH OWNER rd;" &&\
+ # PGPASSWORD=test1234 psql -h 172.20.0.2 -U rd postgres --command "ALTER USER rd WITH SUPERUSER;"
+ 
 npm install -g npm
+
+# nodeenv --node=8.9.4 ~/env/lead-mgmt
+# nodeenv --node=8.9.4 ~/env/lanyard 
+
 
 python tests.py
